bf90b188ab7df8c165ffa12a9a6ffde80f7dda36
TJ Phan <t.jamesphan@gmail.com>
2022-05-12 11:20:29 -0700

    Add mustache

---
+++ index.js
@@ -0,0 +1,30 @@
+
+const Mustache = require('mustache');
+const fs = require('fs');
+const MUSTACHE_MAIN_DIR = './main.mustache';
+
+// Dynamic info in DATA
+let DATA = {
+  name: 'TJ Phan',
+  date: new Date().toLocaleDateString('en-GB', {
+    weekday: 'long',
+    month: 'long',
+    day: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+    timeZoneName: 'short',
+    timeZone: 'America/Vancouver',
+  }),
+};
+/**
+  * Mustache renders file with the data
+  * Creates a README.md file with the generated output
+  */
+function generateReadMe() {
+  fs.readFile(MUSTACHE_MAIN_DIR, (err, data) =>  {
+    if (err) throw err;
+    const output = Mustache.render(data.toString(), DATA);
+    fs.writeFileSync('README.md', output);
+  });
+}
+generateReadMe();
